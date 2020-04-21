3ec42ffd03f081e9b644db789b1a2efc3613f703
Shruti Gada <shrutigada@Shrutis-MacBook-Pro.local>
2020-04-21 19:49:11 +0100

    connect.php changes

--- app/database/connect.php
+++ app/database/connect.php
@@ -9,6 +9,7 @@ $conn = new MySQLi($host, $user, $pass, $db_name);
 
 if ($conn->connect_error){
   die('Database connection error: ' . $conn->connect_error);
+  
 }
 
 // $url = getenv('JAWSDB_URL');
