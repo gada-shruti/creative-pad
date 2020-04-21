89f45838cba29998860636719d81d57c75527c26
Shruti Gada <shrutigada@Shrutis-MacBook-Pro.local>
2020-04-21 13:45:36 +0100

    database connection updated

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,16 +1,31 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db_name = 'blog';
+// $host = 'u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+// $user = 'llq4wg4iw14twa45';
+// $pass = 'd5tttw65i7pumydd';
+// $db_name = 'blog';
+//
+// $conn = new MySQLi($host, $user, $pass, $db_name);
+//
+// if ($conn->connect_error){
+//   die('Database connection error: ' . $conn->connect_error);
+// }
 
-$conn = new MySQLi($host, $user, $pass, $db_name);
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
 
-if ($conn->connect_error){
-  die('Database connection error: ' . $conn->connect_error);
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-//else{
-//  echo "Database connection successful";
-//}
+echo "Connection was successfully established!";
+
  ?>
