e9586e15ad58d442df5e4764e81b2ee6e8b2da4c
Shruti Gada <shrutigada@Shrutis-MacBook-Pro.local>
2020-04-21 16:03:29 +0100

    changes in connet.php

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,31 +1,31 @@
 <?php
 
-// $host = 'u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
-// $user = 'llq4wg4iw14twa45';
-// $pass = 'd5tttw65i7pumydd';
-// $db_name = 'blog';
-//
-// $conn = new MySQLi($host, $user, $pass, $db_name);
-//
-// if ($conn->connect_error){
-//   die('Database connection error: ' . $conn->connect_error);
-// }
+$host = 'db4free.net:3306';
+$user = 'blog_creativepad';
+$pass = 'elsa1234';
+$db_name = 'blog';
 
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
+$conn = new MySQLi($host, $user, $pass, $db_name);
 
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
-
-// Create connection
-$conn = new mysqli($hostname, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error){
+  die('Database connection error: ' . $conn->connect_error);
 }
- echo "Connection was successfully established!";
+
+// $url = getenv('JAWSDB_URL');
+// $dbparts = parse_url($url);
+//
+// $hostname = $dbparts['host'];
+// $username = $dbparts['user'];
+// $password = $dbparts['pass'];
+// $database = ltrim($dbparts['path'],'/');
+//
+// // Create connection
+// $conn = new mysqli($hostname, $username, $password, $database);
+//
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+//  echo "Connection was successfully established!";
 
  ?>
