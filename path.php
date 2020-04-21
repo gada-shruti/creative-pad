f68cc9ad3f17398c60a6f6705e909fbe747dd621
Shruti Gada <shrutigada@Shrutis-MacBook-Pro.local>
2020-04-21 15:06:17 +0100

    path edit

--- path.php
+++ path.php
@@ -2,5 +2,7 @@
 //define a constant variable ROOT_PATH
 define("ROOT_PATH", realpath(dirname(__FILE__)));
 
-define("BASE_URL","http://localhost:8080/blog")
+// define("BASE_URL","http://localhost:8080/blog")
+
+define("BASE_URL","https://creative-pad.herokuapp.com")
  ?>
